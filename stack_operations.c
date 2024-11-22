27d0e93e4661ff39f09ed721e43da50d41b55561
Pratik Bugade <pratikbugade555@gmail.com>
2024-11-22 09:40:24 -0800

    First Commit

---
+++ stack_operations.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+ 
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+ 
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+
+void push(struct stack *ptr,int val)
+{
+    if(isFull(ptr))
+    {
+        printf("stack is overflow,%d is not pushed !!!\n",val);
+    }
+    else{
+        ptr->top++;
+        ptr->arr[ptr->top]=val;
+        printf("%d is pushed \n",ptr->arr[ptr->top]);
+    }
+}
+
+void pop(struct stack * ptr)
+{
+    if(isEmpty(ptr))
+    {
+        printf("stack is underflow");
+    }
+    else{
+        int no=ptr->arr[ptr->top];
+        ptr->top--;
+        printf("%d is poped \n",no);
+    }
+};
+
+
+
+
+ 
+int main()
+{
+    struct stack *s = (struct stack *)malloc(sizeof(struct stack));
+    s->size = 5;
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int));
+    
+    printf("%d\n",isEmpty(s));
+    printf("%d\n",isFull(s));
+
+    push(s,10);
+    push(s,20);
+    push(s,30);
+    push(s,40);
+    push(s,50);
+
+    pop(s);
+    pop(s);
+    pop(s);
+    
+    push(s,100);
+    push(s,1000);
+    push(s,555);
+
+    printf("%d\n",isEmpty(s));
+    printf("%d\n",isFull(s));
+    return 0;
+}
